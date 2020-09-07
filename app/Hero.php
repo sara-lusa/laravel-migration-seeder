281920efcddfa7b81b766a770f2e1c2dad8d4f0f
Sara <saracorsob@gmail.com>
2020-09-07 17:08:54 +0200

    Creazione Model e scrittura Seeder con conseguente completamento db

---
+++ app/Hero.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hero extends Model
+{
+  protected $fillable = [
+    'name',
+    'nickname',
+    'superpower',
+    'age'
+  ];
+}
--- database/seeds/HeroesTableSeeder.php
+++ database/seeds/HeroesTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Hero;
 
 class HeroesTableSeeder extends Seeder
 {
@@ -9,8 +11,23 @@ class HeroesTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i<7 ; $i++) {
+          $new_hero = new Hero();
+          $new_hero->name = $faker->name;
+          $new_hero->nickname = $faker->lastName;
+          $new_hero->superpower = $faker->randomElement([
+            'Volare',
+            'Essere Invisibile',
+            'Leggere la Mente',
+            'Super Forza',
+            'Super Velocità',
+            'Telecinesi',
+            'Controllo degli Elementi'
+          ]);
+          $new_hero->age = $faker->numberBetween(10, 100);
+          $new_hero->save();
+        }
     }
 }
